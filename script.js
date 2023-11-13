bb414dd168b974c55e83cc1be346bb6871806b61
Achu-Prasad <144442978+Achu-Prasad@users.noreply.github.com>
2023-11-13 13:21:29 +0530

    Drum_Simulator (Ver: 1.0 )
    Fully_ Functional & Responsive Design

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
 const drumIcons = document.querySelectorAll(".drums_icon");
 
 for (var i = 0; i < drumIcons.length; i++) {
+    
     drumIcons[i].addEventListener("click", function() {
         const dataKey = this.getAttribute("data-key");
+
+
+        const clickedBorder = document.querySelector(`.drum_icon_border[data-key = "${dataKey}"]`);
+        clickedBorder.classList.add("border");
+        setTimeout(function() {
+            clickedBorder.classList.remove("border");
+        }, 500);
+
+
         const clickAudio = document.querySelector(`audio[data-key="${dataKey}"]`);
         if (!clickAudio) return;
         clickAudio.currentTime = 0;
         clickAudio.play();
-        // Add the "click" class
+
+
         this.classList.add("click");
-        // Remove the "click" class after 500 milliseconds (0.5 seconds)
         setTimeout(function () {
             this.classList.remove("click");
         }.bind(this), 500);
-
-        const border1 = document.querySelectorAll(".drum_icon_border");
-        for (var i=0; i<=border1.length;i++){
-        border1[i].addEventListener("click", function(){
-        this.classList.add("border");
-        setTimeout(function () {
-            this.classList.remove("border");
-        }.bind(this), 500);
-    })
-}
     });
 }
 
 
 
-// const border1 = document.querySelectorAll(".drum_icon_border");
-// for (var i=0; i<=border1.length;i++){
-//     border1[i].addEventListener("click", function(){
-//         this.classList.add("border");
-//         setTimeout(function () {
-//             this.classList.remove("border");
-//         }.bind(this), 500);
-//     })
-// }
 
 
-window.addEventListener('keydown',function(e){
-    const audioSelector = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
-    if(!audioSelector) return;
+window.addEventListener('keydown', function(e) {
+    const audioSelector = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audioSelector) return;
+
+    const pressIcon = document.querySelector(`.drums_icon[data-key="${e.keyCode}"]`);
+    if (!pressIcon) return;
+
     audioSelector.currentTime = 0;
     audioSelector.play();
-    console.log(audioSelector);
-    drumIcons.classList.add("click");
-    setTimeout(function () {
-        drumIcons.classList.remove("click");
-    }.bind(this), 500);
-})
+
+    pressIcon.classList.add("click");
+    setTimeout(function() {
+        pressIcon.classList.remove("click");
+    }, 500);
+
+    const pressedKey = document.querySelector(`.drum_icon_border[ data-key="${e.keyCode}"]`);
+    if(!pressedKey) return;
+    pressedKey.classList.add("border");
+    setTimeout(function(){
+        pressedKey.classList.remove("border");
+    },500);
+});
+
